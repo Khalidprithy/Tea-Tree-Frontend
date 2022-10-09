1309ea66b45b3816b6a962c07a395ae55ea5cdb4
Khalidprithy <khalidprithy@gmail.com>
2022-10-10 05:10:45 +0600

    review updated

--- src/components/Home/HomeReview.js
+++ src/components/Home/HomeReview.js
@@ -7,7 +7,7 @@ const HomeReview = () => {
     return (
         <div className='bg-white'>
             <h1 className='text-2xl font-bold text-center py-6 mx-2'>What Our Customer Says</h1>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mx-4 pb-10'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 w-11/12 lg:w-10/12 mx-auto pb-10'>
                 {
                     reviews.slice(0, 3).map(review => <ReviewCard
                         key={review._id}
